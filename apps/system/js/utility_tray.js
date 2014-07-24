2728d397cff66381fb066ed2042ba6034fa5d9b2
Etienne Segonzac <etienne@segonzac.info>
2014-07-24 12:18:40 +0200

    Bug 1042083 - Implement App Titlebar
    (cherry picked from commit f622c144ac746fcbf08e9fe65d750bca6ba6a56c)

--- apps/system/js/utility_tray.js
+++ apps/system/js/utility_tray.js
@@ -19,6 +19,8 @@ var UtilityTray = {
 
   statusbarIcons: document.getElementById('statusbar-icons'),
 
+  topPanel: document.getElementById('top-panel'),
+
   grippy: document.getElementById('utility-tray-grippy'),
 
   screen: document.getElementById('screen'),
@@ -29,6 +31,7 @@ var UtilityTray = {
       this.overlay.addEventListener(name, this);
       this.statusbarIcons.addEventListener(name, this);
       this.grippy.addEventListener(name, this);
+      this.topPanel.addEventListener(name, this);
     }, this);
 
     window.addEventListener('screenchange', this);
@@ -65,6 +68,7 @@ var UtilityTray = {
 
   startY: undefined,
   lastDelta: undefined,
+  isTap: false,
   screenWidth: 0,
   screenHeight: 0,
   grippyHeight: 0,
@@ -117,7 +121,8 @@ var UtilityTray = {
         }
 
         if (target !== this.overlay && target !== this.grippy &&
-            evt.currentTarget !== this.statusbarIcons) {
+            evt.currentTarget !== this.statusbarIcons &&
+            evt.currentTarget !== this.topPanel) {
           return;
         }
 
@@ -217,8 +222,8 @@ var UtilityTray = {
                                   [touch.rotationAngle], [touch.force], 1);
       }
     }
-    this.screen.classList.add('utility-tray');
-    this.notifications.classList.add('visible');
+
+    this.isTap = true;
 
     window.dispatchEvent(new CustomEvent('utility-tray-overlayopening'));
   },
@@ -235,6 +240,13 @@ var UtilityTray = {
 
     var dy = -(this.startY - y);
     this.lastDelta = dy;
+
+    // Tap threshold
+    if (dy > 5) {
+      this.isTap = false;
+      this.screen.classList.add('utility-tray');
+      this.notifications.classList.add('visible');
+    }
 
     if (this.shown) {
       dy += screenHeight;
@@ -263,6 +275,7 @@ var UtilityTray = {
     }
     this.startY = undefined;
     this.lastDelta = undefined;
+    this.isTap = false;
   },
 
   hide: function ut_hide(instant) {
@@ -323,5 +336,3 @@ var UtilityTray = {
     }
   }
 };
-
-UtilityTray.init();
