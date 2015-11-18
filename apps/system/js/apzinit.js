0cd115d4c7594783f38364718a7e4e07f6ca0a5e
Etienne Segonzac <etienne@segonzac.info>
2015-11-18 11:57:41 +0100

    Shake to reload.

--- apps/system/js/apzinit.js
+++ apps/system/js/apzinit.js
@@ -8,4 +8,33 @@ window.addEventListener('load', function() {
   apzFrame.style.width = apzFrame.style.height = '100%';
   apzFrame.src = '/apz-gambler/index.html';
   document.body.appendChild(apzFrame);
+
+  // Shake to reload
+  var EXCITEMENT_FILTER_ALPHA = 0.2;
+  var EXCITEMENT_THRESHOLD = 500;
+  var DEVICE_MOTION_EVENT = 'devicemotion';
+  var SCREEN_CHANGE_EVENT = 'screenchange';
+
+  var screenEnabled = true;
+  var excitement = 0;
+
+  window.addEventListener(SCREEN_CHANGE_EVENT, function(evt) {
+    screenEnabled = evt.detail.screenEnabled;
+    if (screenEnabled) {
+      window.addEventListener(DEVICE_MOTION_EVENT, handleMotion);
+    } else {
+      window.removeEventListener(DEVICE_MOTION_EVENT, handleMotion);
+    }
+  });
+
+  function handleMotion(evt) {
+    let acc = evt.accelerationIncludingGravity;
+
+    let newExcitement = acc.x * acc.x + acc.y * acc.y + acc.z * acc.z;
+    excitement += (newExcitement - excitement) * EXCITEMENT_FILTER_ALPHA;
+
+    if (excitement > EXCITEMENT_THRESHOLD) {
+      apzFrame.src = '/apz-gambler/index.html';
+    }
+  }
 });
