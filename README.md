0a8ef40d3ff1ffa3bc8da6833d763b6520b203c7
Etienne Segonzac <etienne@segonzac.info>
2015-12-15 10:31:08 +0100

    Moving to the "host" model in preparation for the addon.

--- README.md
+++ README.md
@@ -1,25 +1,17 @@
 # Step 1
-Checkout the APZ-gambler in the directory where gaia is checked out.
+Checkout hope in the directory where gaia is checked out.
 
 ```shell
-git clone https://github.com/etiennesegonzac/APZ-gambler.git apz-gambler
+git clone https://github.com/etiennesegonzac/hope.git
 ls
-> gaia apz-gambler
+> gaia hope
 ```
 
 # Step 2
-Make the APZ-gambler ready for full screen, comment [those
-lines](https://github.com/etiennesegonzac/APZ-gambler/blob/f3b18f5671580da85a27d1292424d7ce62be56e5/css/style.css#L18-L19).
-
-# Step 3
 Flash the alternate system app
 
 ```shell
 cd gaia
 adb root
-NOFTU=1 DEVICE_DEBUG=1 make reset-gaia
+APP=system make install-gaia
 ```
-
-# Emergency step
-If anything goes wrong during a demo, just shake the phone to reload the
-prototype.
--- apps/system/js/apzinit.js
+++ apps/system/js/apzinit.js
@@ -1,40 +1,55 @@
 window.addEventListener('load', function() {
-  document.getElementById('screen').style.opacity = '0.01';
   var apzFrame = document.createElement('iframe');
   apzFrame.style.border = '0';
-  apzFrame.style.zIndex = '10000000000';
+  apzFrame.style.zIndex = '10000';
   apzFrame.style.position = 'absolute';
-  apzFrame.style.top = apzFrame.style.left = apzFrame.style.right = apzFrame.style.bottom = '0';
-  apzFrame.style.width = apzFrame.style.height = '100%';
-  apzFrame.src = '/apz-gambler/index.html';
+  apzFrame.style.top = 'var(--statusbar-height)';
+  apzFrame.style.left = apzFrame.style.right = apzFrame.style.bottom = '0';
+  apzFrame.style.width = '100%';
+  apzFrame.style.height = 'calc(100% - var(--statusbar-height) - var(--software-home-button-height))';
+  apzFrame.src = '/hope/index.html';
   document.body.appendChild(apzFrame);
 
-  // Shake to reload
-  var EXCITEMENT_FILTER_ALPHA = 0.2;
-  var EXCITEMENT_THRESHOLD = 500;
-  var DEVICE_MOTION_EVENT = 'devicemotion';
-  var SCREEN_CHANGE_EVENT = 'screenchange';
+  // Statusbar color
+  var sheet = document.createElement('style');
+  sheet.setAttribute('type', 'text/css');
 
-  var screenEnabled = true;
-  var excitement = 0;
+  var styleText = document.createTextNode([
+    '#statusbar { background: #333333 !important }',
+    '#windows .appWindow .titlebar { background: #333333 !important }',
+    '#software-home-ring { outline: none; background: no-repeat center/70% url(/hope/assets/Home.png); }',
+    '#hope-back {',
+    'position: absolute;',
+    'top: 0;',
+    'left: 1rem;',
+    'height: 5rem;',
+    'width: 7rem;',
+    'padding: 0;',
+    'pointer-events: all;',
+    'background: transparent;',
+    'background-image: url(/hope/assets/Back.png);',
+    'background-repeat: no-repeat;',
+    'background-size: 23px 27px;',
+    'background-position: center center;',
+    'border: none;',
+    '}',
+  ].join('\n'));
 
-  window.addEventListener(SCREEN_CHANGE_EVENT, function(evt) {
-    screenEnabled = evt.detail.screenEnabled;
-    if (screenEnabled) {
-      window.addEventListener(DEVICE_MOTION_EVENT, handleMotion);
-    } else {
-      window.removeEventListener(DEVICE_MOTION_EVENT, handleMotion);
-    }
+  sheet.appendChild(styleText);
+  document.head.appendChild(sheet);
+
+  // Home button support
+  window.addEventListener('home', function() {
+    apzFrame.contentWindow.dispatchEvent(new CustomEvent('home-button-press'));
   });
 
-  function handleMotion(evt) {
-    let acc = evt.accelerationIncludingGravity;
+  // Back button support
+  var buttons = document.getElementById('software-buttons');
+  var back = document.createElement('button');
+  back.id = 'hope-back';
+  buttons.insertBefore(back, buttons.firstElementChild);
 
-    let newExcitement = acc.x * acc.x + acc.y * acc.y + acc.z * acc.z;
-    excitement += (newExcitement - excitement) * EXCITEMENT_FILTER_ALPHA;
-
-    if (excitement > EXCITEMENT_THRESHOLD) {
-      apzFrame.src = '/apz-gambler/index.html';
-    }
-  }
+  back.addEventListener('click', function() {
+    apzFrame.contentWindow.dispatchEvent(new CustomEvent('back-button-press'));
+  });
 });
